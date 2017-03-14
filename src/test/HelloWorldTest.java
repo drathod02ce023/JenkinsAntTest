a39b5c57a3f7a1ceafc38187d8833d7ce8ff1c93
Dhaval Rathod <drathod@jp.ibm.com>
2017-03-14 15:08:36 +0900

    AntTest

---
+++ src/test/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package test;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		System.out.println("Ant Test successfull");
+	}
+
+}
---
+++ src/test/HelloWorldTest2.java
@@ -0,0 +1,13 @@
+package test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest2 {
+
+	@Test
+	public void test() {
+		System.out.println("Ant Test successfull");
+	}
+
+}
